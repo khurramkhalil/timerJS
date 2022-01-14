843f6aee9561566de6635f810869cf7186d843f8
Khurram Khalil <kkhali.pg@smme.edu.pk>
2022-01-13 22:52:32 -0800

    Added complete functioning timer with start pause and stop at zero check.

--- js/index.js
+++ js/index.js
@@ -4,22 +4,62 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener('click', this.start)
-    this.pauseButton.addEventListener('click', this.pause)
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  };
+
+  start = () => {
+
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
   }
 
-  start() {
-    console.log('Time to start the Timer!!')
+  set timeRemaining(time) {
+    this.durationInput.value = time;
   }
-  pause() {
-    console.log("Pause the execution!!")
-  }
+
 }
 
 
-const durationInput = document.getElementById('input')
+const durationInput = document.getElementById('duration')
 const startButton = document.getElementById('start')
 const pauseButton = document.getElementById('pause')
 
 
 timer = new Timer(durationInput, startButton, pauseButton)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
