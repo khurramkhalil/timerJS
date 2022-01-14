1a0c2e2f4dbd8359e354abdf3dcd90374f3a5ca1
Khurram Khalil <kkhali.pg@smme.edu.pk>
2022-01-13 23:09:55 -0800

    Created code to communicate with outside world via callbacks.

--- js/index.js
+++ js/index.js
@@ -1,49 +1,19 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  };
-
-  start = () => {
-
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-
-}
-
-
 const durationInput = document.getElementById('duration')
 const startButton = document.getElementById('start')
 const pauseButton = document.getElementById('pause')
 
 
-timer = new Timer(durationInput, startButton, pauseButton)
+timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer Started!!")
+  },
+  onTick() {
+    console.log("Timer just ticked down")
+  },
+  onComplete() {
+    console.log("Timer completed!!")
+  }
+})
 
 
 
