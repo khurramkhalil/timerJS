8f48139553badf1fbefac1a2ac43703fe7eb79e6
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-17 02:03:15 -0800

    Fully working animated Timer with user input.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const durationInput = document.getElementById('duration')
 const startButton = document.getElementById('start')
 const pauseButton = document.getElementById('pause')
-const circle = document.getElementById('circle')
+const circle = document.querySelector('circle')
 const perimeter = circle.getAttribute('r') * 2 * Math.PI
 circle.setAttribute('stroke-dasharray', perimeter)
 
